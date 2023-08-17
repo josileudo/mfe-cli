441b0b8b7843d4435d69d7ace9c2e585acc31888
josileudo <josileudorodrigues75@gmail.com>
2023-08-17 18:25:10 -0300

    :sparkles: feat: Create progressbar

--- bin/cli.js
+++ bin/cli.js
@@ -3,12 +3,18 @@
 const {execSync} = require('child_process');
 const fs = require('fs');
 const path = require('path');
-
+//const { createPromptModule } = require('inquirer');
 const ex = require('fs-extra');
+const { SingleBar, Presets } = require('cli-progress');
 
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).argv;
+
+let progress;
+
+// const yargs = require('yargs/yargs');
+// const { hideBin } = require('yargs/helpers');
+// const argv = yargs(hideBin(process.argv)).argv;
+
+// ASK questions
 
 const removeFilesRequired = async (src, dest) => {
   try {
@@ -20,7 +26,7 @@ const removeFilesRequired = async (src, dest) => {
 
 const runCommand = command => {
   try {
-    execSync(`${command}`, {stdio: 'inherit'});
+    execSync(`${command}`, {stdio: ['inherit', 'ignore', 'ignore']});
   } catch (e) {
     console.error(`Failed to execute ${command}`, e);
     return false;
@@ -29,103 +35,133 @@ const runCommand = command => {
   return true;
 }
 
-// Check all structure to change name project.
-// const replaceAllOccurrences = (obj, searchValue, replaceValue) => {
-//   for(const key in obj) {
-//     if(typeof obj[key] === 'object') {
-//       replaceAllOccurrences(obj[key], searchValue, replaceValue);
-//     } else if (typeof obj[key] === 'string') {
-//       obj[key] = obj[key].replace(new RegExp(searchValue, 'g'), replaceValue);
-//     }
-//   }
-// }
-
-// Will be receive a name to replace
-// const updateJsonFile = (basePath, fileName, projectName) => {
-//   const filePath = path.join(basePath, fileName);
-
-//   if(fs.existsSync(filePath)) {
-//     const fileJsonContent = fs.readFileSync(filePath, 'utf-8');
-//     const fileJson = JSON.parse(fileJsonContent);
-
-//     if(fileJson.projects) {
-//       fileJson.projects[projectName] = fileJson.projects['create-mfe-app'];
-//       delete fileJson.projects['create-mfe-app'];
-//     }
-
-//     const oldName = 'create-mfe-app'
-//     replaceAllOccurrences(fileJson, oldName, projectName);
-
-//     fs.writeFileSync(filePath, JSON.stringify(fileJson, null, 2), 'utf-8');
-//   }
-// }
-
-const updateNameInFiles = (basePath, fileName, oldName, projectName) => {
-  const filePath = path.join(basePath, fileName);
-  let fileContent = fs.readFileSync(filePath, 'utf-8');
-  
-  fileContent = fileContent.replace(new RegExp(oldName, 'g'), projectName);
-
-  fs.writeFileSync(filePath, fileContent, 'utf-8');
+const progressConfig = (message) => {  
+  progress = new SingleBar({
+    format: `${message}: [{bar}] {percentage}% | ETA: {eta}s | {value}/{total} Steps`,
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
+  }, Presets.shades_classic);
 }
 
-const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 --branch 15.0.x https://github.com/josileudo/create-mfe-app ${repoName}`;
-const installAfterCommand = `cd ${repoName} && npm i -f`;
+const main = async() => {
+  try {
+    const { createPromptModule } = await import('inquirer');
+ 
+    const prompt = createPromptModule();
+    const answers = await prompt([
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Input your name prefer: '
+      },
+      {
+        type: 'list',
+        name: 'style',
+        message: 'Choice your style extension: ',
+        choices: ['scss', 'css', 'sass', 'less']
+      }
+    ])
 
-console.log(`Cloning the repository with name ${repoName}`);
-const checkedOut = runCommand(gitCheckoutCommand);
-if(!checkedOut) process.exit(-1);
+    const repoName = answers.projectName;
+    const gitCheckoutCommand = `git clone --depth 1 --branch 15.0.x https://github.com/josileudo/create-mfe-app ${repoName}`;
+    const installAfterCommand = `cd ${repoName} && npm i -f`;
+    
+    // console.warn()
 
-const projectName = repoName;
-const templatePath = path.join(process.cwd(), repoName);
-const oldName = 'create-mfe-app';
+    progressConfig(`${'\x1b[33m'} Creating ${repoName} project`)
 
-// Replace *TS files
-updateNameInFiles(templatePath, 'src/app/app.component.ts', oldName, projectName);
-updateNameInFiles(templatePath, 'src/app/app.component.spec.ts', oldName, projectName);
-updateNameInFiles(templatePath, 'src/app/app.module.ts', oldName, projectName);
+    // start the progress bar with a total value of 200 and start value of 0
+    progress.start(100, 0);
 
-// Replace *Json files
-updateNameInFiles(templatePath, 'package.json', oldName, projectName);
-updateNameInFiles(templatePath, 'angular.json', oldName, projectName);
+    // update the current value in your application..
+    progress.update(20);
 
-// get versions
+    // stop the progress bar
+          
+    // const checkedOut = runCommand(gitCheckoutCommand);
+    // if(!checkedOut) process.exit(-1);
+    execSync(gitCheckoutCommand, { stdio: ['inherit', 'ignore', 'ignore' ]})
+    
+    progress.update(80);
 
+    const projectName = repoName;
+    const templatePath = path.join(process.cwd(), repoName);
+    const oldName = 'create-mfe-app';
 
-// Choice style
-const choiceStyleExtension = () => {
-  const extensions = ['scss', 'css', 'sass', 'less'];
+    const updateNameInFiles = (basePath, fileName, oldName, projectName) => {
+      const filePath = path.join(basePath, fileName);
+      let fileContent = fs.readFileSync(filePath, 'utf-8');
+      
+      fileContent = fileContent.replace(new RegExp(oldName, 'g'), projectName);
+    
+      fs.writeFileSync(filePath, fileContent, 'utf-8');
+    }
 
-  let res = extensions.filter((extension) => extension === argv.style);
-  
-  res.map(type => {
-    if(type)  {
-      const oldExt = path.join(templatePath, 'src', 'styles.scss');
-      const newExt = path.join(templatePath, 'src', `styles.${type}`);
-
-      updateNameInFiles(templatePath, 'angular.json', 'scss', type);
-
-      fs.rename(oldExt, newExt, (err) => {
-        if(err) console.error('Extensions error!!!!');
+    // Replace *TS files
+    updateNameInFiles(templatePath, 'src/app/app.component.ts', oldName, projectName);
+    updateNameInFiles(templatePath, 'src/app/app.component.spec.ts', oldName, projectName);
+    updateNameInFiles(templatePath, 'src/app/app.module.ts', oldName, projectName);
+    
+    // Replace *Json files
+    updateNameInFiles(templatePath, 'package.json', oldName, projectName);
+    updateNameInFiles(templatePath, 'angular.json', oldName, projectName);
+    
+    // get versions    
+    
+    // Choice style
+    const choiceStyleExtension = () => {
+      const extensions = ['scss', 'css', 'sass', 'less'];
+    
+      let res = extensions.filter((extension) => extension === answers.style);
+      
+      res.map(type => {
+        if(type)  {
+          const oldExt = path.join(templatePath, 'src', 'styles.scss');
+          const newExt = path.join(templatePath, 'src', `styles.${type}`);
+    
+          updateNameInFiles(templatePath, 'angular.json', 'scss', type);
+    
+          fs.rename(oldExt, newExt, (err) => {
+            if(err) console.error(`${'x1b[31m'} Extensions error!!!!`);
+          })
+        }
+        return;
       })
     }
-    return;
-  })
+    
+    choiceStyleExtension();
+    
+    // Replace *README.md name
+    updateNameInFiles(templatePath, 'README.md', oldName, projectName);
+    
+    // Remove .git file to initializing a new project
+    removeFilesRequired(projectName, '.git');
+
+    progress.update(100);
+
+    progress.stop();
+
+    progressConfig(`${'\x1b[33m'} Installing dependencies for the ${'\x1b[32m', repoName} project`)
+    
+    progress.start(100, 0);
+
+    const installedDeps = runCommand(installAfterCommand);
+    
+    if(!installedDeps) process.exit(-1);
+    progress.update(80);
+    
+    // Work finish
+    progress.update(100);
+    progress.stop();
+
+    console.log(`${'\x1b[32m'} Congratulations! You are ready. Follow the following commands to start`);
+    console.warn(`${'\x1b[34m'} cd ${repoName} && npm start`);  
+
+  } catch(err) {
+    console.error(`${'\x1b[31m'}An error ocurred: `, err)
+    process.exit(1);
+  }
 }
 
-choiceStyleExtension();
-
-// Replace *README.md name
-updateNameInFiles(templatePath, 'README.md', oldName, projectName);
-
-// Remove .git file to initializing a new project
-removeFilesRequired(projectName, '.git');
-
-console.log(`Installing dependencies for the ${repoName}`);
-const installedDeps = runCommand(installAfterCommand);
-if(!installedDeps) process.exit(-1);
-
-// Work finish
-console.warn("Congratulations! You are ready. Follow the following commands to start");
-console.warn(`cd ${repoName} && npm start`);
+main();
